af75c60bf90a3f429a1d1886770abb75185c52ec
Kenzie Togami <ket1999@gmail.com>
2014-03-07 15:42:11 -0800

    Remove FBN

--- elmodule/platform/PlatformModule.java
+++ elmodule/platform/PlatformModule.java
@@ -4,15 +4,8 @@ import emergencylanding.k.exst.modules.IModule;
 import emergencylanding.k.library.main.KMain;
 
 public class PlatformModule extends IModule {
-    private static final String FULL_BINARY_NAME = PlatformModule.class
-            .getName();
 
     @Override
     public void init(KMain instance) {
     }
-
-    public static String getFullBinaryName() {
-        return FULL_BINARY_NAME;
-    }
-
 }
